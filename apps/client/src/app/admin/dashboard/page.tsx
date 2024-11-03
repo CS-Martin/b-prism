27bb1d2f9f25c738e0942a8d065a4c564802e3b0
martin <meatole.cs@gmail.com>
2024-11-03 19:05:44 +0800

    feat: added admin dashboard

--- apps/client/src/app/admin/dashboard/page.tsx
+++ apps/client/src/app/admin/dashboard/page.tsx
@@ -2,14 +2,73 @@ import { getServerSession } from 'next-auth';
 import { options } from '../../api/auth/[...nextauth]/options';
 import { UserDto } from '@dto';
 
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@b-prism/ui-components';
+import { userService } from 'apps/client/src/services/user-service';
+
 type User = UserDto;
 
 export default async function AdminDashboard() {
     const session = await getServerSession(options);
 
+    const response = (await userService.findAll()) || [];
+
+    // @ts-ignore
+    const users = response.body.map((user: UserDto) => ({
+        id: user.id,
+        given_name: user.given_name,
+        family_name: user.family_name,
+        email: user.email,
+        office: user.office,
+        position: user.position,
+        id_image_url: user.id_image_url,
+        role: user.role,
+    }));
+
     return (
         <div>
             <h1>Admin Dashboard</h1>
+
+            <Table>
+                <TableCaption>A list of your recent invoices.</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">#</TableHead>
+                        <TableHead>Name</TableHead>
+                        <TableHead>Surename</TableHead>
+                        <TableHead>Email</TableHead>
+                        <TableHead>Office</TableHead>
+                        <TableHead>Position</TableHead>
+                        <TableHead>ID Image</TableHead>
+                        <TableHead>Role</TableHead>
+                        <TableHead className="text-right">Actions</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {users.map((user: User, index: number) => (
+                        <TableRow key={user.id}>
+                            <TableCell>{index + 1}</TableCell>
+                            <TableCell>{user.given_name}</TableCell>
+                            <TableCell>{user.family_name}</TableCell>
+                            <TableCell>{user.email}</TableCell>
+                            <TableCell>{user.office}</TableCell>
+                            <TableCell>{user.position}</TableCell>
+                            <TableCell>{user.id_image_url}</TableCell>
+                            <TableCell>{user.role}</TableCell>
+                            <TableCell className="text-right">
+                                $250.00
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
 
             {session?.user ? (
                 <div>
--- apps/client/ui-components/src/index.ts
+++ apps/client/ui-components/src/index.ts
@@ -1,2 +1,3 @@
 export * from './components/ui/button';
 export * from './components/ui/accordion';
+export * from './components/ui/table';
