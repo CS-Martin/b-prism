a0e371b74495915b185ab7b74fcdbb58a786ffef
martin <meatole.cs@gmail.com>
2024-11-05 23:35:44 +0800

    chore: removed path to commit lint config

--- path/to/commitlint.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    rules: {
-        'header-max-length': [2, 'always', 72],
-        'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'perf', 'test']],
-    },
-}; 
