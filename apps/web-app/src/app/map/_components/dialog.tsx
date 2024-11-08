488337a8b51e66a1a34d37e9d74c2ada3fb7dffb
martin <meatole.cs@gmail.com>
2024-11-08 11:34:34 +0800

    fix: lint error on textarea component

---
+++ apps/web-app/src/app/map/_components/dialog.tsx
@@ -0,0 +1,119 @@
+'use client';
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+    Input,
+    Label,
+    Textarea,
+} from '@b-prism/shadcn-ui/index';
+import { CreateWarehouseDto } from '@dto';
+import { warehouseService } from 'apps/web-app/src/services/warehouse.service';
+import { useState, useEffect } from 'react';
+
+interface MarkerType {
+    longitude: string;
+    latitude: string;
+}
+
+interface DialogProps {
+    isOpen: boolean;
+    setIsOpen: (isOpen: boolean) => void;
+    marker: MarkerType;
+    fetchAllWarehouses: () => void;
+}
+
+const MapDialog: React.FC<DialogProps> = ({ isOpen, setIsOpen, marker, fetchAllWarehouses }) => {
+    const [warehouseData, setWarehouseData] = useState<CreateWarehouseDto>({
+        name: '',
+        description: '',
+        longitude: marker.longitude,
+        latitude: marker.latitude,
+        capacity: 0,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    });
+
+    useEffect(() => {
+        setWarehouseData({ ...warehouseData, longitude: marker.longitude, latitude: marker.latitude });
+    }, [marker]);
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setWarehouseData({ ...warehouseData, [name]: value });
+    };
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        await warehouseService.create(warehouseData);
+
+        fetchAllWarehouses();
+
+        setIsOpen(false);
+    };
+
+    return (
+        <>
+            <Dialog
+                open={isOpen}
+                onOpenChange={setIsOpen}
+            >
+                <DialogTrigger>Open</DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Warehouse Information</DialogTitle>
+                        <DialogDescription>Please enter the details of the warehouse.</DialogDescription>
+                    </DialogHeader>
+                    <div className='grid gap-4'>
+                        <form
+                            method='POST'
+                            onSubmit={handleSubmit}
+                        >
+                            <div>
+                                <Label className='mb-5'>Name</Label>
+                                <Input
+                                    id='name'
+                                    name='name'
+                                    className='rounded-sm mt-1'
+                                    onChange={handleInputChange}
+                                    placeholder='Warehouse Name'
+                                />
+                            </div>
+
+                            <div>
+                                <Label className=''>Description</Label>
+                                <Textarea
+                                    id='description'
+                                    name='description'
+                                    className='rounded-sm mt-1'
+                                    onChange={handleInputChange}
+                                    placeholder='Description'
+                                />
+                            </div>
+
+                            <div className='items-center'>
+                                <Label className=''>Capacity</Label>
+                                <Input
+                                    id='capacity'
+                                    name='capacity'
+                                    type='number'
+                                    className='rounded-sm mt-1'
+                                    onChange={handleInputChange}
+                                    placeholder='Ex. 1000'
+                                />
+                            </div>
+                            <button type='submit'>Submit</button>
+                        </form>
+                    </div>
+                </DialogContent>
+            </Dialog>
+        </>
+    );
+};
+
+export default MapDialog;
--- apps/web-app/src/app/map/_components/mapbox.tsx
+++ apps/web-app/src/app/map/_components/mapbox.tsx
@@ -1,7 +1,44 @@
-import Map from 'react-map-gl';
+import Map, { Marker, MapMouseEvent } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { useState } from 'react';
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+    Input,
+    Label,
+    Textarea,
+} from '@b-prism/shadcn-ui/index';
+import { CreateWarehouseDto } from '@dto';
+import MapDialog from './dialog';
+import { useDisplayWarehouses } from 'apps/web-app/src/hooks/map.hook';
+
+interface MarkerType {
+    longitude: string;
+    latitude: string;
+}
 
 const Mapbox = () => {
+    const [marker, setMarker] = useState<MarkerType>({ longitude: '', latitude: '' });
+    const [isOpen, setIsOpen] = useState(false);
+
+    const { warehouses, isLoading, fetchAllWarehouses } = useDisplayWarehouses();
+
+    console.log(warehouses);
+
+    const handleMapClick = (event: MapMouseEvent) => {
+        const longitude: string = event.lngLat.lng.toString();
+        const latitude: string = event.lngLat.lat.toString();
+
+        setMarker({ longitude, latitude });
+
+        setIsOpen(true);
+    };
+
     return (
         <div id='map'>
             <Map
@@ -16,7 +53,24 @@ const Mapbox = () => {
                 }}
                 style={{ position: 'absolute', width: '100%', height: 'calc(100vh - 28px)' }}
                 mapStyle={process.env.NEXT_PUBLIC_MAPBOX_STYLE}
-            />
+                onClick={handleMapClick}
+            >
+                <MapDialog
+                    isOpen={isOpen}
+                    setIsOpen={setIsOpen}
+                    marker={marker}
+                    fetchAllWarehouses={fetchAllWarehouses}
+                />
+                {warehouses.map((warehouse, index) => (
+                    <Marker
+                        key={index}
+                        longitude={Number(warehouse.longitude)}
+                        latitude={Number(warehouse.latitude)}
+                    >
+                        <div style={{ backgroundColor: 'red', width: '10px', height: '10px', borderRadius: '50%' }} />
+                    </Marker>
+                ))}
+            </Map>
         </div>
     );
 };
