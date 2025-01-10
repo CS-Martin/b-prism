f86460598c8caea22bdbacd48d499b4a52aa9b5f
martin <meatole.cs@gmail.com>
2025-01-10 13:43:16 +0800

    feat: used flex-col-reverse and ordered data by 'desc

--- apps/web-app/src/app/admin/activity-logs/_components/activity-card.tsx
+++ apps/web-app/src/app/admin/activity-logs/_components/activity-card.tsx
@@ -4,7 +4,7 @@ import { format, formatDistanceToNow } from 'date-fns';
 
 export default function ActivityCard(log: ActivityLogDto) {
     return (
-        <div className='ml-6 hover:bg-[#27272a] w-full h-[85%] rounded-md px-3 py-2 cursor-pointer'>
+        <div className='ml-6 hover:bg-[#27272a] w-full h-fit min-h-[115px] max-h-[135px] px-3 py-2 rounded-md cursor-pointer'>
             <p className='text-[12px] text-[#a1a1aa]'>
                 {format(new Date(log.timestamp), 'MMMM d, yyyy')} | {formatDistanceToNow(new Date(log.timestamp))} ago
             </p>
--- apps/web-app/src/app/admin/activity-logs/page.tsx
+++ apps/web-app/src/app/admin/activity-logs/page.tsx
@@ -16,9 +16,20 @@ export default function ActivityLogging() {
     console.log(logs);
 
     useEffect(() => {
-        // Scrolldown on render
-        scrollAreaRef.current?.scrollIntoView({ behavior: 'smooth' });
+        // Scroll to the bottom on initial render and when logs update
+        if (scrollAreaRef.current) {
+            scrollAreaRef.current.scrollTop = scrollAreaRef.current.scrollHeight;
+        }
     }, [logs]);
+
+    const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
+        const element = e.currentTarget;
+
+        // If the user scrolls to the bottom, load more logs
+        if (element.scrollTop === -(element.scrollHeight - element.clientHeight)) {
+            setVisibleLogs((prev) => Math.min(prev + 10, logs.length));
+        }
+    };
 
     if (isLoading) {
         return <div>Loading...</div>;
@@ -34,22 +45,22 @@ export default function ActivityLogging() {
             />
 
             <div className='bg-[#18181A] rounded-md py-6 w-[80%] mt-6'>
-                {/* If scroll hits the top, show 'show more' button */}
-                <ScrollArea className='relative h-[45rem] px-6'>
-                    {/* Timeline Items */}
-                    {logs.map((log, index) => (
+                <div
+                    className='relative h-[45rem] px-6 flex flex-col-reverse overflow-y-scroll'
+                    ref={scrollAreaRef}
+                    onScroll={handleScroll}>
+                    {logs.slice(0, visibleLogs).map((log, index) => (
                         <div
                             key={index}
-                            ref={scrollAreaRef}
-                            className='relative flex items-start h-[120px] rounded-md'>
+                            className='relative flex items-start h-fit rounded-md'>
                             {/* Circle and Line */}
                             <div className='relative flex flex-col items-center h-full py-2'>
                                 {/* Circle */}
                                 {ActionIcon(log.action)}
-
                                 {/* Line below the circle */}
-                                {index < logs.length - 1 && <div className='h-[60%] rounded-full w-[0.7px] bg-gray-500 absolute top-11'></div>}
+                                {index > 0 && <div className='h-[60%] rounded-full w-[0.7px] bg-gray-500 absolute top-11'></div>}
                             </div>
+
                             <div className='ml-10 w-full text-center py-2 max-w-[120px]'>
                                 <p className='text-[#a1a1aa] text-[12px]'>{format(new Date(log.timestamp), 'hh:mm a')}</p>
                             </div>
@@ -57,7 +68,7 @@ export default function ActivityLogging() {
                             {ActivityCard(log)}
                         </div>
                     ))}
-                </ScrollArea>
+                </div>
             </div>
         </div>
     );
