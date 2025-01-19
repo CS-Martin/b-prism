edf6e06dcd9c308aef2b844625c7de3e70fc09d6
martin <meatole.cs@gmail.com>
2025-01-19 13:31:48 +0800

    feat: applied new card dynamic styles to pages

--- apps/web-app/src/app/admin/activity-logs/_components/activity-card.tsx
+++ apps/web-app/src/app/admin/activity-logs/_components/activity-card.tsx
@@ -4,7 +4,7 @@ import { format, formatDistanceToNow } from 'date-fns';
 
 export default function ActivityCard(log: ActivityLogDto) {
     return (
-        <div className='ml-6 hover:bg-[#27272a] w-full h-fit min-h-[115px] max-h-[135px] px-3 py-2 rounded-md cursor-pointer'>
+        <div className='ml-6 prism-card-hover w-full h-fit min-h-[115px] max-h-[135px] px-3 py-2 rounded-md cursor-pointer'>
             <p className='text-[12px] text-[#a1a1aa]'>
                 {format(new Date(log.timestamp), 'MMMM d, yyyy')} | {formatDistanceToNow(new Date(log.timestamp))} ago
             </p>
