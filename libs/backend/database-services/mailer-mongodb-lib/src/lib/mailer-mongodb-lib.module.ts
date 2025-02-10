b09bacc1e58ec353bc8b2ea96f3a4af79bbf92ea
martin <meatole.cs@gmail.com>
2025-02-10 19:42:44 +0800

    feat: created createMailerDto and completed mailermongodblib service

--- libs/backend/database-services/mailer-mongodb-lib/src/lib/mailer-mongodb-lib.module.ts
+++ libs/backend/database-services/mailer-mongodb-lib/src/lib/mailer-mongodb-lib.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MailerMongodbLibService } from './mailer-mongodb-lib.service';
+import { PrismaDbLibModule } from '@prisma-db-lib';
 
 @Module({
     controllers: [],
+    imports: [PrismaDbLibModule],
     providers: [MailerMongodbLibService],
     exports: [MailerMongodbLibService],
 })
--- libs/backend/database-services/mailer-mongodb-lib/src/lib/mailer-mongodb-lib.service.ts
+++ libs/backend/database-services/mailer-mongodb-lib/src/lib/mailer-mongodb-lib.service.ts
@@ -1,7 +1,27 @@
+import { CreateMailerDto } from '@dto';
 import { Injectable } from '@nestjs/common';
 import { PrismaDbLibService } from '@prisma-db-lib';
+import { Mailer } from '@prisma/client';
 
 @Injectable()
 export class MailerMongodbLibService {
     constructor(private readonly prisma: PrismaDbLibService) {}
+
+    async create(createMailerDto: CreateMailerDto): Promise<Mailer> {
+        const mailer = this.prisma.mailer.create({
+            data: {
+                ...createMailerDto,
+            },
+        });
+
+        return mailer;
+    }
+
+    async delete(id: string): Promise<void> {
+        this.prisma.mailer.delete({
+            where: {
+                id,
+            },
+        });
+    }
 }
