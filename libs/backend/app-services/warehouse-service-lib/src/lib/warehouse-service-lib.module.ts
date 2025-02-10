1c94c35e278513010848e2fe38a8c668d18444a5
martin <meatole.cs@gmail.com>
2025-02-11 01:58:26 +0800

    fix: push build errors wrong import path

--- libs/backend/app-services/warehouse-service-lib/src/lib/warehouse-service-lib.module.ts
+++ libs/backend/app-services/warehouse-service-lib/src/lib/warehouse-service-lib.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { WarehouseServiceLibService } from './warehouse-service-lib.service';
 import { WarehouseMongodbLibModule } from '@b-prism/warehouse-mongodb-lib';
-import { ActivityLogServiceLibModule } from '@b-prisma/activity-log-service-lib';
+import { ActivityLogServiceLibModule } from '@b-prism/activity-log-service-lib';
 
 @Module({
     imports: [WarehouseMongodbLibModule, ActivityLogServiceLibModule],
