96ab6625e6eba8b7fabc2e4881ee6b77368d668c
martin <meatole.cs@gmail.com>
2025-02-11 16:15:54 +0800

    feat: downgraded chokidar and install shadcn input-otp

--- libs/shadcn-ui/src/index.ts
+++ libs/shadcn-ui/src/index.ts
@@ -28,3 +28,4 @@ export * from './components/ui/navigation-menu';
 export * from './components/ui/scroll-area';
 export * from './components/ui/collapsible';
 export * from './components/ui/tabs';
+export * from './components/ui/input-otp';
