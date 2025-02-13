f700ad0d0a57db105b765cb25c88d290367639c4
martin <meatole.cs@gmail.com>
2025-02-13 19:30:48 +0800

    style: further improved ui of landing page

--- apps/web-app/src/app/home/page.tsx
+++ apps/web-app/src/app/home/page.tsx
@@ -1,27 +1,16 @@
 'use client';
 
-import Spline from '@splinetool/react-spline';
 import Link from 'next/link';
 
 import { Button } from '@b-prism/shadcn-ui/index';
 import { useRef } from 'react';
 import { ArrowDown } from 'lucide-react';
 import { useSession } from 'next-auth/react';
-import { SupportersParallax } from './_components/parallax-text';
-import { motion, useScroll, useTransform } from 'motion/react';
+import { SplineSection } from './_components/spline-section';
 
 export default function Home() {
     const { data: session } = useSession();
     const bottomRef = useRef<HTMLDivElement>(null);
-    const staticRef = useRef(null);
-
-    const { scrollYProgress } = useScroll({
-        target: staticRef,
-        offset: ['start start', 'end start'],
-    });
-
-    const splineBackground = useTransform(scrollYProgress, [0, 1], ['1%', '-50%']);
-    const splineText = useTransform(scrollYProgress, [0, 1], ['10%', '-200%']);
 
     const scrollToBottom = () => {
         bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
@@ -29,38 +18,7 @@ export default function Home() {
 
     return (
         <div className='mt-[100px] '>
-            <section className='relative overflow-hidden px-[75px] h-[100vh]'>
-                <div className=''>
-                    <motion.div
-                        style={{ y: splineBackground }}
-                        className='h-[80vh] '>
-                        <Spline
-                            className='rounded-2xl shadow-2xl neon-border'
-                            scene='https://prod.spline.design/ng-ni62YpZyz95ES/scene.splinecode'
-                        />
-                        <div className='absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black rounded-2xl'></div>
-
-                        <motion.div
-                            style={{ y: splineText }}
-                            className='absolute inset-0 flex h-[80%] flex-col items-center top-[10px] text-center mt-14'>
-                            <p className='text-[40px] font-black'>
-                                Unite for Resilience, <br />
-                                Act with Precision
-                            </p>
-                            <p className='text-lg mt-3 text-gray-400'>
-                                Coordinate rescue efforts, strengthen disaster response, and <br /> safeguard communities with our all-in-one platform.
-                            </p>
-                        </motion.div>
-                    </motion.div>
-
-                    <section className=' mt-5'>
-                        <SupportersParallax />
-                    </section>
-                </div>
-
-                {/* I want a running horizontal icons of  */}
-            </section>
-
+            <SplineSection />
             <section className='h-[100vh]'></section>
 
             {session?.user ? (
