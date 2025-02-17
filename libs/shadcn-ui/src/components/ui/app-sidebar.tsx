c14d85e8e0338b3e12a9509d6b273db766adff1f
martin <meatole.cs@gmail.com>
2025-02-18 02:16:21 +0800

    style: added logo in sidebar

--- libs/shadcn-ui/src/components/ui/app-sidebar.tsx
+++ libs/shadcn-ui/src/components/ui/app-sidebar.tsx
@@ -31,6 +31,7 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSepar
 import { UserDto } from '@dto';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
+import Image from 'next/image';
 
 interface AppSidebarProps {
     setSelectedAction: (action: string | null) => void;
@@ -174,7 +175,26 @@ export function AppSidebar({ setSelectedAction }: AppSidebarProps) {
         <Sidebar
             variant='floating'
             collapsible='icon'>
-            <SidebarHeader>LOGO</SidebarHeader>
+            <SidebarHeader className='pt-5'>
+                <SidebarMenuButton
+                    size='lg'
+                    className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'>
+                    {/* className={`transition-all duration-300 ease-in-out bg-blue-600 rounded-sm flex items-center justify-center ${state === 'collapsed' ? '' : 'h-10 w-10'}`}> */}
+                    <div className='flex size-11 items-center justify-center shadow-xl rounded-lg bg-blue-600 text-sidebar-primary-foreground'>
+                        <Image
+                            src={'/logo/haribon-logo.svg'}
+                            height={24}
+                            width={24}
+                            alt='haribon logo'
+                            className={`${state === 'collapsed' ? 'size-6' : 'size-7'}`}
+                        />
+                    </div>
+                    <div className={`grid flex-1 text-left text-sm leading-relaxed ${state === 'collapsed' ? 'hidden' : 'block'}`}>
+                        <span className='truncate text-xs'>Project</span>
+                        <span className='truncate font-bold'>HARIBON</span>
+                    </div>
+                </SidebarMenuButton>
+            </SidebarHeader>
 
             <SidebarContent>
                 {/* General Links */}
