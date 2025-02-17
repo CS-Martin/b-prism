65fbc28636d765ebc8bd07d6180f4185c2ac3710
martin <meatole.cs@gmail.com>
2025-02-18 01:23:05 +0800

    style: sidebar items

--- libs/shadcn-ui/src/components/ui/app-sidebar.tsx
+++ libs/shadcn-ui/src/components/ui/app-sidebar.tsx
@@ -6,6 +6,9 @@ import {
     Avatar,
     AvatarFallback,
     AvatarImage,
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
     DropdownMenuItem,
     ModeToggle,
     Sidebar,
@@ -18,19 +21,22 @@ import {
     SidebarMenu,
     SidebarMenuButton,
     SidebarMenuItem,
+    SidebarMenuSub,
     Switch,
+    useSidebar,
 } from '@b-prism/shadcn-ui/index';
 import { useState } from 'react';
-import { ChevronUp, History, Home, LayoutDashboard, LogIn, LogOut, LucideIcon, Map, MapPinned, MapPinPlus, MapPinXInside, MonitorCog } from 'lucide-react';
+import { ChevronDown, ChevronUp, History, Home, LayoutDashboard, LogIn, LogOut, LucideIcon, Map, MapPinned, MapPinPlus, MapPinXInside, MonitorCog } from 'lucide-react';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@b-prism/shadcn-ui/index';
 import { UserDto } from '@dto';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 interface AppSidebarProps {
     setSelectedAction: (action: string | null) => void;
 }
 
-const Links = [
+const generalLinks = [
     {
         id: 'home',
         label: 'Home',
@@ -39,7 +45,7 @@ const Links = [
     },
     {
         id: 'dashboard',
-        label: 'Dashboard',
+        label: 'Resources Dashboard',
         icon: LayoutDashboard,
         href: '/dashboard',
     },
@@ -49,6 +55,9 @@ const Links = [
         icon: Map,
         href: '/map',
     },
+];
+
+const adminLinks = [
     {
         id: 'admin-dashboard',
         label: 'Admin Dashboard',
@@ -63,7 +72,7 @@ const Links = [
     },
 ];
 
-const actions = [
+const mapActions = [
     {
         id: 'createWarehouse',
         label: 'Create Warehouse',
@@ -145,13 +154,14 @@ export function AppSidebar({ setSelectedAction }: AppSidebarProps) {
     const { toast } = useToast();
     const user: UserDto = session?.user as UserDto;
     const [selectedAction, setInternalSelectedAction] = useState<string | null>(null);
+    const { state } = useSidebar();
 
     const handleToggle = (action: string) => {
         const newAction = selectedAction === action ? null : action;
         setInternalSelectedAction(newAction);
         setSelectedAction(newAction);
 
-        const actionData = actions.find((a) => a.id === newAction);
+        const actionData = mapActions.find((a) => a.id === newAction);
         if (actionData) {
             toast({
                 title: actionData.toastTitle,
@@ -167,36 +177,129 @@ export function AppSidebar({ setSelectedAction }: AppSidebarProps) {
             <SidebarHeader>LOGO</SidebarHeader>
 
             <SidebarContent>
+                {/* General Links */}
                 <SidebarGroup>
-                    <SidebarGroupLabel>Links</SidebarGroupLabel>
-                    <SidebarGroupContent>
-                        <SidebarMenu className='gap-2'>
-                            {Links.map((link) => (
-                                <SidebarLinkItem
-                                    key={link.id}
-                                    {...link}
-                                />
-                            ))}
-                        </SidebarMenu>
-                    </SidebarGroupContent>
+                    <SidebarMenu>
+                        <Collapsible
+                            defaultOpen
+                            className='group/collapsible'>
+                            <SidebarMenuItem>
+                                <CollapsibleTrigger asChild>
+                                    <SidebarMenuButton
+                                        style={{ backgroundColor: 'transparent' }}
+                                        variant='default'
+                                        className={`${state === 'collapsed' ? 'hidden' : 'flex'}`}>
+                                        <SidebarGroupLabel className='px-0'>General</SidebarGroupLabel>
+                                        <ChevronDown className='ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180' />
+                                    </SidebarMenuButton>
+                                </CollapsibleTrigger>
+                                <CollapsibleContent asChild>
+                                    <motion.div
+                                        initial={{ height: 0, opacity: 0 }}
+                                        animate={{ height: 'auto', opacity: 1 }}
+                                        exit={{ height: 0, opacity: 0 }}
+                                        transition={{ duration: 0.3 }}
+                                        className='overflow-hidden'>
+                                        <SidebarMenu className='gap-2'>
+                                            {generalLinks.map((link) => (
+                                                <SidebarLinkItem
+                                                    key={link.id}
+                                                    {...link}
+                                                />
+                                            ))}
+                                        </SidebarMenu>
+                                    </motion.div>
+                                </CollapsibleContent>
+                            </SidebarMenuItem>
+                        </Collapsible>
+                    </SidebarMenu>
                 </SidebarGroup>
+
+                {/* When sidebar state is close, display a horizontal bar or line to separate the link group */}
+                <hr className={`border-t ${state === 'collapsed' ? 'mx-[20%]' : 'mx-[5%]'}`} />
+
+                {/* Admin Links */}
                 <SidebarGroup>
-                    <SidebarGroupLabel style={{ display: user?.role !== 'admin' && user?.role !== 'verified' ? 'none' : 'block' }}>Actions</SidebarGroupLabel>
-                    <SidebarGroupContent>
-                        <SidebarMenu className='gap-2'>
-                            {actions.map((action) => (
-                                <SidebarActionItem
-                                    key={action.id}
-                                    id={action.id}
-                                    label={action.label}
-                                    icon={action.icon}
-                                    selectedAction={selectedAction}
-                                    handleToggle={handleToggle}
-                                />
-                            ))}
-                        </SidebarMenu>
-                    </SidebarGroupContent>
+                    <SidebarMenu>
+                        <Collapsible
+                            defaultOpen
+                            className='group/collapsible'>
+                            <SidebarMenuItem>
+                                <CollapsibleTrigger asChild>
+                                    <SidebarMenuButton
+                                        style={{ backgroundColor: 'transparent' }}
+                                        variant='default'
+                                        className={`${state === 'collapsed' ? 'hidden' : 'flex'}`}>
+                                        <SidebarGroupLabel className='px-0'>Admin</SidebarGroupLabel>
+                                        <ChevronDown className='ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180' />
+                                    </SidebarMenuButton>
+                                </CollapsibleTrigger>
+                                <CollapsibleContent>
+                                    <motion.div
+                                        initial={{ height: 0, opacity: 0 }}
+                                        animate={{ height: 'auto', opacity: 1 }}
+                                        exit={{ height: 0, opacity: 0 }}
+                                        transition={{ duration: 0.3 }}
+                                        className='overflow-hidden'>
+                                        <SidebarMenu className='gap-2'>
+                                            {adminLinks.map((link) => (
+                                                <SidebarLinkItem
+                                                    key={link.id}
+                                                    {...link}
+                                                />
+                                            ))}
+                                        </SidebarMenu>
+                                    </motion.div>
+                                </CollapsibleContent>
+                            </SidebarMenuItem>
+                        </Collapsible>
+                    </SidebarMenu>
                 </SidebarGroup>
+
+                <hr className={`border-t ${state === 'collapsed' ? 'mx-[20%]' : 'mx-[5%]'}`} />
+
+                {(user?.role === 'admin' || user?.role === 'verified') && (
+                    <SidebarGroup>
+                        <SidebarMenu>
+                            <Collapsible
+                                defaultOpen
+                                className='group/collapsible'>
+                                <SidebarMenuItem>
+                                    <CollapsibleTrigger asChild>
+                                        <SidebarMenuButton
+                                            style={{ backgroundColor: 'transparent' }}
+                                            variant='default'
+                                            className={`${state === 'collapsed' ? 'hidden' : 'flex'}`}>
+                                            <SidebarGroupLabel className='px-0'>Map Actions</SidebarGroupLabel>
+                                            <ChevronDown className='ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180' />
+                                        </SidebarMenuButton>
+                                    </CollapsibleTrigger>
+                                    <CollapsibleContent>
+                                        <motion.div
+                                            initial={{ height: 0, opacity: 0 }}
+                                            animate={{ height: 'auto', opacity: 1 }}
+                                            exit={{ height: 0, opacity: 0 }}
+                                            transition={{ duration: 0.3 }}
+                                            className='overflow-hidden'>
+                                            <SidebarMenu className='gap-2'>
+                                                {mapActions.map((action) => (
+                                                    <SidebarActionItem
+                                                        key={action.id}
+                                                        id={action.id}
+                                                        label={action.label}
+                                                        icon={action.icon}
+                                                        selectedAction={selectedAction}
+                                                        handleToggle={handleToggle}
+                                                    />
+                                                ))}
+                                            </SidebarMenu>
+                                        </motion.div>
+                                    </CollapsibleContent>
+                                </SidebarMenuItem>
+                            </Collapsible>
+                        </SidebarMenu>
+                    </SidebarGroup>
+                )}
             </SidebarContent>
             <SidebarFooter>
                 <div className='flex items-center justify-between p-5 border-2 border-gray-400 rounded-xl border-dashed group-data-[state=collapsed]:hidden'>
@@ -217,8 +320,8 @@ export function AppSidebar({ setSelectedAction }: AppSidebarProps) {
                                 </SidebarMenuButton>
                             </DropdownMenuTrigger>
                             <DropdownMenuContent
-                                side='right'
-                                align='start'
+                                side='top'
+                                align='end'
                                 className='w-[--radix-popper-anchor-width]'
                                 style={{ borderRadius: 'var(--radius)' }}>
                                 <DropdownMenuLabel>My Account</DropdownMenuLabel>
