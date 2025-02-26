a4f1bf7463906e60c147fced3355c184f698fdee
martin <meatole.cs@gmail.com>
2025-02-26 22:36:35 +0800

    feat: added searchbar and filter by action in activity log

--- libs/shadcn-ui/src/index.ts
+++ libs/shadcn-ui/src/index.ts
@@ -32,3 +32,4 @@ export * from './components/ui/tabs';
 export * from './components/ui/input-otp';
 export * from './components/ui/card';
 export * from './components/ui/alert';
+export * from './components/ui/menubar';
