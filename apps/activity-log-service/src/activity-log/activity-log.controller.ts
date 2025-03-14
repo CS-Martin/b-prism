1a075550c563128157cec9ebe13e89c798d02734
martin <meatole.cs@gmail.com>
2025-03-14 16:26:46 +0800

    refactor(api-endpoints): standardize activitylog endpoints

--- apps/activity-log-service/src/activity-log/activity-log.controller.ts
+++ apps/activity-log-service/src/activity-log/activity-log.controller.ts
@@ -1,16 +1,17 @@
 import { ActivityLogServiceLibService } from '@b-prism/activity-log-service-lib';
 import { CreateActivityLogDto } from '@dto';
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from 'libs/backend/app-services/guards-service-lib/src/lib/jwt-auth.guard';
 
 @ApiTags('Activity Log Endpoints')
 @UseGuards(AuthGuard)
-@Controller('activity-log')
+@Controller(`${new ConfigService().get<string>('API_VERSION')}/activity-log`)
 export class ActivityLogController {
     constructor(private readonly activityLogServiceLibService: ActivityLogServiceLibService) {}
 
-    @Post('create')
+    @Post()
     create(@Body() data: CreateActivityLogDto) {
         return this.activityLogServiceLibService.create(data);
     }
