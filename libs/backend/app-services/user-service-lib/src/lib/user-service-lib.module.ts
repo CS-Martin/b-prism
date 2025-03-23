bda89101daffba5ca3b0678386e05a75a88e97c4
martin <meatole.cs@gmail.com>
2025-03-23 11:15:40 +0800

    feat(role/api): created new endpoint for updating user role

--- libs/backend/app-services/user-service-lib/src/lib/user-service-lib.module.ts
+++ libs/backend/app-services/user-service-lib/src/lib/user-service-lib.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserServiceLibService } from './user-service-lib.service';
 import { UserMongodbLibModule } from '@b-prism/user-mongodb-lib';
+import { ActivityLogServiceLibModule } from '@b-prism/activity-log-service-lib';
 
 @Module({
     controllers: [],
-    imports: [UserMongodbLibModule],
+    imports: [UserMongodbLibModule, ActivityLogServiceLibModule],
     providers: [UserServiceLibService],
     exports: [UserServiceLibService],
 })
