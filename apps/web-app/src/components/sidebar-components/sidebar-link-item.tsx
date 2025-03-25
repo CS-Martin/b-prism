5ba9db0e156414316f2b63b29e4150edb0d10319
martin <meatole.cs@gmail.com>
2025-03-25 19:13:57 +0800

    style: sidebar button active states

--- apps/web-app/src/components/sidebar-components/sidebar-link-item.tsx
+++ apps/web-app/src/components/sidebar-components/sidebar-link-item.tsx
@@ -1,5 +1,6 @@
 import { SidebarMenuButton, SidebarMenuItem } from '@b-prism/shadcn-ui/index';
 import { LucideIcon } from 'lucide-react';
+import { usePathname } from 'next/navigation';
 
 interface SidebarLinkItemProps {
     label: string;
@@ -8,9 +9,16 @@ interface SidebarLinkItemProps {
 }
 
 export const SidebarLinkItem = ({ label, icon: Icon, href }: SidebarLinkItemProps) => {
+    const pathname = usePathname();
+    const isActive = pathname === href;
+
     return (
         <SidebarMenuItem>
-            <SidebarMenuButton asChild>
+            <SidebarMenuButton
+                asChild
+                size='default'
+                className='data-[active=true]:bg-blue-500 data-[active=true]:hover:bg-blue-600 data-[active=true]:text-white'
+                isActive={isActive}>
                 <a
                     href={href}
                     className='flex items-center'>
