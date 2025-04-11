03ab1b9056336fa2cd293402450eeb70e0dc43c3
martin <meatole.cs@gmail.com>
2025-04-11 14:49:32 +0800

    fix(production): internal server error

--- apps/web-app/src/app/map/page.tsx
+++ apps/web-app/src/app/map/page.tsx
@@ -1,23 +1,14 @@
 'use client';
 
-import { getServerSession } from 'next-auth';
-import { options } from '../api/auth/[...nextauth]/options';
-import { MapboxContext } from './_components/mapbox';
-import { cookies } from 'next/headers';
-import { NextStep, NextStepProvider } from 'nextstepjs';
 import { useSession } from 'next-auth/react';
+import dynamic from 'next/dynamic';
+
+const MapboxContext = dynamic(() => import('./_components/mapbox').then((mod) => mod.MapboxContext), {
+    ssr: false,
+});
 
 export default function MapPage() {
     const { data: session } = useSession();
-    // const cookie = await cookies();
-
-    // Check if user first time login
-    // If first time, start onboarding
-    // const firstMapVisit = cookie.get('firstMapVisit');
-    // if (!firstMapVisit) {
-    //     cookie.set('firstMapVisit', 'true', { path: '/', maxAge: 60 * 30 });
-    // } else {
-    // }
 
     return <MapboxContext session={session} />;
 }
